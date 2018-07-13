724bcf52089754efee9c8294aeded69301f86544
helto4real <tomas.hellstrom@yahoo.se>
2018-07-13 10:23:05 +0200

    removed hard-coded

--- appdaemon/apps/rooms/room_tvroom.py
+++ appdaemon/apps/rooms/room_tvroom.py
@@ -12,21 +12,21 @@ class TVRoom(Area):
     def initialize(self) -> None:
         super().initialize()
         
-        self._remote = self.args.get('remote', str)
- 
+        self._remote = self.properties.get('remote', str)
+        self._toggle_tv_switch = self.properties.get('toggle_tv_switch', str)
+        self._toggle_window_ligts_switch = self.properties.get('toggle_window_ligts_switch', str)
+        
     def on_lightswich_state_changed(self, entity: str, old: str, new: str)->None:
         super().on_lightswich_state_changed(entity, old, new)
-        if entity=='switch.switch_1_lb':
+        if entity==self._toggle_window_ligts_switch:
             # turn on/off lights in window
             self.__toogle_window_lights()
-        elif entity=='switch.switch_1_rb':
+        elif entity==self._toggle_tv_switch:
             self.__toogle_tv()
-
 
     def __toogle_tv(self)->None:
         self.toggle(entity_id=self._remote)
     
     def __toogle_window_lights(self)->None:
-        
         self.toggle(entity_id='light.tvnere_fonster')
     
