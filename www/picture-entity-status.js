742fb77df53186e0e9a32e6e494a7dbd03f7a62d
helto4real <tomas.hellstrom@yahoo.se>
2018-07-19 19:46:45 +0200

    Added dark theme and custom presence lovelace card i wrote

--- www/picture-entity-status.js
+++ www/picture-entity-status.js
@@ -1,61 +1,132 @@
-import {html} from 'https://unpkg.com/@polymer/polymer/lib/utils/html-tag.js';
+/*
+    Custom card to display presence information on a image.
+    The style is optimized for my specific height running 4xtiles horisontally
 
-const HuiPictureEntityCard  = customElements.get('hui-picture-entity-card');
-//const html = customElements.get('html');
-class PictureEntityStatus extends HuiPictureEntityCard {
-  static get template() {
-    return html`
-      <style>
+    if you want to change nr of tiles, change the scale variable to greater value than 50px, 100px for two tiles, 75 for 3 maybe
+        if (!cardConfig.scale) cardConfig.scale = "50px";
+    then adjust the css as you have to to compensate for size change. Sorry for this hard coded stuff but i suck at javascript and HTML5 :)
+
+      - type: custom:picture-entity-status
+        entity: sensor.presence_tomas
+        image: /local/tomas_presence_away.jpg #standard image for away
+        state_image:
+            Home: /local/tomas_presence.jpg             # for each state specify image
+            Just arrived: /local/tomas_presence.jpg     # for each state specify image
+            
+*/
+
+
+class PictureEntityStatus extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+  setConfig(config) {
+    if (!config.entity) {
+        throw new Error('Please define an entity');
+    }
+
+    const root = this.shadowRoot;
+    if (root.lastChild) root.removeChild(root.lastChild);
+
+    const cardConfig = Object.assign({}, config);
+    
+    if (!cardConfig.scale) cardConfig.scale = "50px";
+    if (!cardConfig.min) cardConfig.min = 0;
+    if (!cardConfig.max) cardConfig.max = 100;
+
+    const card = document.createElement('ha-card');
+    const shadow = card.attachShadow({ mode: 'open' });
+    const content = document.createElement('div');
+    const style = document.createElement('style');
+
+    content.innerHTML = `
+    <div class="container" id="container">
+        <div id="image"></div>
+        <div id="shadow"></div>
+        <div id="title">Home</div>
+    </div>
+    `;
+
+    style.textContent = `
         ha-card {
-          min-height: 75px;
-          overflow: hidden;
-          position: relative;
+        --base-unit: ${cardConfig.scale};
+        height: calc(var(--base-unit)*2);
+        position: relative;
         }
-        ha-card.canInteract {
-          cursor: pointer;
+        .container{
+        width: calc(var(--base-unit) * 2.42);
+        height: calc(var(--base-unit) * 2.0);
+        position: absolute;
+        top: 0px;
+        
+        overflow: hidden;
+        text-align: center;
+        
         }
-        .info {
-          @apply --paper-font-common-nowrap;
-          position: absolute;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: rgba(0, 0, 0, 0.3);
-          padding: 16px;
-          font-size: 16px;
-          line-height: 16px;
-          color: white;
-          display: flex;
-          justify-content: space-between;
+        .container #image {
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: 50% 50%;
+        }
+        #shadow {
+            width: 100%;
+            height: 25%;
+            position: relative;
+            left: 0px;
+            vertical-align: bottom;
+            top: calc(-100%/4);
+            background-color: black;
+            opacity: 0.5;
+            
+            
         }
         #title {
-          font-weight: 500;
-        }
-        [hidden] {
-          display: none;
-        }
-      </style>
-      <ha-card id='card' on-click="_cardClicked">
-        <hui-image
-          hass="[[hass]]"
-          image="[[_config.image]]"
-          state-image="[[_config.state_image]]"
-          camera-image="[[_getCameraImage(_config)]]" 
-          entity="[[_config.entity]]"
-        ></hui-image>
-        <div class="info" hidden$='[[_computeHideInfo(_config)]]'>
-          <div id="kalle" style="font-size: 35px;">&nbsp;</div>
-          <div id="state"></div>
-          <div id="name"></div>
-          <div id="bertil" style="font-size: 5px;" >&nbsp;</div>
-          </div>
-      </ha-card>
-    `; // 
-  }
-  setConfig(config) {
-    super.setConfig(config)
-  }
+            width: 100%;
+            height: 100%;
+            color: white;
+            text-align: center;
+            font-weight: bold;
+            font-size: calc(var(--base-unit)/4);
+            position: relative;
+            top: calc(-100%/2.18);
  
+        }
+    `;
+    card.appendChild(content);
+    card.appendChild(style);
+    card.addEventListener('click', event => {
+      this._fire('hass-more-info', { entityId: cardConfig.entity });
+    });
+    root.appendChild(card);
+    this._config = cardConfig;
+  }
+  set hass(hass) {
+    const config = this._config;
+    const entityState = hass.states[config.entity].state;
+
+    const root = this.shadowRoot;
+ 
+    var imgUrl = `url(${config.image})`;
+    if (entityState !== this._entityState) {
+        console.log(config.state_image);
+        for (var key in config.state_image) {
+            if (config.state_image.hasOwnProperty(key)) {           
+                if (key == entityState)
+                    imgUrl = `url(${config.state_image[key]})`;
+            }
+        }
+        root.getElementById("title").textContent = entityState;
+        
+    }
+    root.getElementById("image").style.backgroundImage = imgUrl;
+    root.lastChild.hass = hass; 
+  }
+  getCardSize() {
+    return 1;
+  }
 }
 
 customElements.define('picture-entity-status', PictureEntityStatus);
---
+++ www/picture-entity-status_inheritage.js
@@ -0,0 +1,67 @@
+/*
+  Testa card to try out inherit standard card.. Not using. Ended up to write my own custom
+
+*/
+
+import {html} from 'https://unpkg.com/@polymer/polymer/lib/utils/html-tag.js';
+
+const HuiPictureEntityCard  = customElements.get('hui-picture-entity-card');
+//const html = customElements.get('html');
+
+class PictureEntityStatus extends HuiPictureEntityCard {
+  static get template() {
+    return html`
+      <style>
+        ha-card {
+          min-height: 75px;
+          overflow: hidden;
+          position: relative;
+        }
+        ha-card.canInteract {
+          cursor: pointer;
+        }
+        .info {
+          @apply --paper-font-common-nowrap;
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: rgba(0, 0, 0, 0.3);
+          padding: 16px;
+          font-size: 16px;
+          line-height: 16px;
+          color: white;
+          display: flex;
+          justify-content: space-between;
+        }
+        #title {
+          font-weight: 500;
+        }
+        [hidden] {
+          display: none;
+        }
+      </style>
+      <ha-card id='card' on-click="_cardClicked">
+        <hui-image
+          hass="[[hass]]"
+          image="[[_config.image]]"
+          state-image="[[_config.state_image]]"
+          camera-image="[[_getCameraImage(_config)]]" 
+          entity="[[_config.entity]]"
+        ></hui-image>
+        <div class="info" hidden$='[[_computeHideInfo(_config)]]'>
+          <div id="kalle" style="font-size: 35px;">&nbsp;</div>
+          <div id="state"></div>
+          <div id="name"></div>
+          <div id="bertil" style="font-size: 5px;" >&nbsp;</div>
+          </div>
+      </ha-card>
+    `; // 
+  }
+  /*setConfig(config) {
+    if (super.setConfig)
+      super.setConfig(config);
+  }*/
+ 
+}
+customElements.define('picture-entity-status', PictureEntityStatus);
