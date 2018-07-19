ce1a0735477b3c90e35a4332fcf2c8b08d8b51fa
helto4real <tomas.hellstrom@yahoo.se>
2018-07-19 19:51:58 +0200

    fixed click to view details

--- www/picture-entity-status.js
+++ www/picture-entity-status.js
@@ -103,6 +103,20 @@ class PictureEntityStatus extends HTMLElement {
     root.appendChild(card);
     this._config = cardConfig;
   }
+  _fire(type, detail, options) {
+    const node = this.shadowRoot;
+    options = options || {};
+    detail = (detail === null || detail === undefined) ? {} : detail;
+    const event = new Event(type, {
+      bubbles: options.bubbles === undefined ? true : options.bubbles,
+      cancelable: Boolean(options.cancelable),
+      composed: options.composed === undefined ? true : options.composed
+    });
+    event.detail = detail;
+    node.dispatchEvent(event);
+    return event;
+  }
+  
   set hass(hass) {
     const config = this._config;
     const entityState = hass.states[config.entity].state;
